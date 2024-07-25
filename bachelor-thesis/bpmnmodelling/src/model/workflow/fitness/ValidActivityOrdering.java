9589727c3e06ea7b1452518ec6f4a2cef14eca45
Paul Groß <paul.gross01@web.de>
2024-07-26 00:03:02 +0200

    Remove .xmi files, that only have been used for testing

--- bachelor-thesis/bpmnmodelling/src/model/workflow/fitness/ValidActivityOrdering.java
+++ bachelor-thesis/bpmnmodelling/src/model/workflow/fitness/ValidActivityOrdering.java
@@ -5,13 +5,22 @@ import java.util.List;
 
 import model.workflow.Activity;
 import model.workflow.InformationObject;
-import model.workflow.Task;
 import model.workflow.Workflow;
 import uk.ac.kcl.inf.mdeoptimiser.libraries.core.optimisation.IGuidanceFunction;
 import uk.ac.kcl.inf.mdeoptimiser.libraries.core.optimisation.interpreter.guidance.Solution;
 
 public class ValidActivityOrdering implements IGuidanceFunction {
 	
+	/**
+	 * Calculate, if the workflow has a valid ordering of activities
+	 * Loop over all activities one by one. Gather every Task in the activity,
+	 * if a task has no source, it is a starting activity and can be marked as done
+	 * if a task is dependent on other tasks, then these tasks already need to
+	 * be in the tasksDone list.
+	 * If this is not the case, the outOfOrderCounter is increased
+	 * If the activities are ordered, the outOfOrderCounter will be 0,
+	 * then a valid ordering is given.
+	 */
 	@Override
 	public double computeFitness(Solution solution) {
 		Workflow workflow = (Workflow) solution.getModel();
